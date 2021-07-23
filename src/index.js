7c332333dc890bcb4ea93a8774756d1ee89dcca2
Corey Bailey <public@coreybailey.net>
2021-07-22 20:02:05 -0400

    Add BrowserRouter; Reorder css import.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
 import store from './redux/store'
 
-import './index.css';
-
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
+import './index.css';
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
