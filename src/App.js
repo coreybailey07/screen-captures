1e7ebf12cf27e7168a5a9b5f6357d43057870d48
Corey Bailey <public@coreybailey.net>
2021-07-28 10:03:36 -0400

    Change layout; Remove place holder code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Route, Switch, Link } from 'react-router-dom';
 
 import './App.scss';
@@ -6,31 +5,25 @@ import './App.scss';
 import B2Buy from './component/portfolio/b2buy.component';
 import FedBid from './component/portfolio/fedbid.component';
 
-class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className='app'>
-        <nav className='nav'>
-          <Link className='link' to='/screen-captures/b2buy'>B2Buy</Link>
-          <Link className='link' to='/screen-captures/marketplace'>Marketplace</Link>
-          <Link className='link' to='/screen-captures/fedboss'>FedBoss</Link>
-          <Link className='link' to='/screen-captures/vao'>VAO</Link>
-        </nav>
-        <main>
-          <Switch>
-    {/*<Route exact path='/screen-captures/b2buy/' render={() => (<Portfolio portfolioPath='images/portfolio2/'/>)} />*/}
-            <Route exact path='/screen-captures/b2buy/' render={() => (<B2Buy b2buyPath='images/portfolio2/'/>)} />
-          </Switch>
-        </main>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className='app'>
+      <nav className='nav'>
+        <Link className='link' to='/screen-captures/b2buy'>B2Buy</Link>
+        <Link className='link' to='/screen-captures/fedbid'>FedBid</Link>
+      </nav>
+      <main>
+        <Switch>
+          <Route exact path='/screen-captures/b2buy'>
+            <B2Buy b2buyPath='images/portfolio2/' />
+          </Route>
+          <Route exact path='/screen-captures/fedbid'>
+            <FedBid fedbidPath='images/portfolio2/' />
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  );
 }
 
 export default App;
